5960ee9a09e55efafe8604579dc84e2589af2e91
Jesse James & bobellis <student@epicodus.com>
2015-04-21 13:26:19 -0700

    added code to return score for entire word

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -3,6 +3,10 @@ require('scrabble_score.rb')
 
 describe('String#scrabble_score') do
   it('accepts a letter and returns the scrabble score value of that letter') do
-  expect(("a").scrabble_score()).to(eq(1))
+  expect("a".scrabble_score()).to(eq(1))
+  end
+
+  it('accepts a word and returns the scrabble score value of that word') do
+    expect("be".scrabble_score()).to(eq(4))
   end
 end
