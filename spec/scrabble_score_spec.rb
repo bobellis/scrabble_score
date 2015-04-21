1e5cf42cb22ad4023146923f396239dd51192ed5
Jesse James & bobellis <student@epicodus.com>
2015-04-21 13:37:18 -0700

    Updated RSPEC for mixed case situation test

--- spec/scrabble_score_spec.rb
+++ spec/scrabble_score_spec.rb
@@ -9,4 +9,8 @@ describe('String#scrabble_score') do
   it('accepts a word and returns the scrabble score value of that word') do
     expect("be".scrabble_score()).to(eq(4))
   end
+
+  it('accepts words with upper and lower case characters') do
+    expect("aPPleS".scrabble_score()).to(eq(10))
+  end
 end
